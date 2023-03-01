c3cf23a18c1540aaafcc5b7172df381d404aceba
fatima <fxtimaahmed14@gmail.com>
2023-03-01 11:56:28 +0000

    working on home page

--- task-app/src/App.js
+++ task-app/src/App.js
@@ -1,10 +1,28 @@
 import "./App.css";
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter, Route, Link } from "react-router-dom";
+import Login from "./pages/Login";
+import Signup from "./pages/Signup";
+import Home from "./pages/Home";
 
 function App() {
   return (
     <BrowserRouter>
-      <div></div>
+      <div>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/login">Login</Link>
+          </li>
+          <li>
+            <Link to="/signup">Signup</Link>
+          </li>
+        </ul>
+        <Route path="/" component={Home} />
+        <Route path="/login" component={Login} />
+        <Route path="/signup" component={Signup} />
+      </div>
     </BrowserRouter>
   );
 }
