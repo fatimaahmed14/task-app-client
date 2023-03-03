41b297ef436a84a3eb562285886773f6a770017d
fatima <fxtimaahmed14@gmail.com>
2023-03-03 14:48:47 +0000

    react spring stlying work done on home page

--- task-app/src/pages/Home.js
+++ task-app/src/pages/Home.js
@@ -1,7 +1,46 @@
+import { useState } from "react";
+import { useSpring, animated } from "react-spring";
 import { Link } from "react-router-dom";
 import "../style/App.css";
 
+//   return (
+//     <div className="home">
+//       <animated.nav style={navProps}>
+//         <ul>
+//           <li className="login">
+//             <Link to="/login">Login</Link>
+//           </li>
+//           <li className="signup">
+//             <Link to="/signup">Signup</Link>
+//           </li>
+//         </ul>
+//       </animated.nav>
+//       <animated.main>
+//         <animated.h2 className="title" style={titleProps}>
+//           List it out.
+//         </animated.h2>
+//         <animated.h3 className="slogan" style={sloganProps}>
+//           let it all out... *DIGITALLY*
+//         </animated.h3>
+//       </animated.main>
+//     </div>
+//   );
+// }
+
 function Home() {
+  const [isList, setIsList] = useState(true);
+
+  const titleProps = useSpring({
+    to: {
+      opacity: 1,
+      transform: `translateY(${isList ? "0px" : "-50px"})`,
+    },
+  });
+
+  const toggleList = () => {
+    setIsList(!isList);
+  };
+
   return (
     <div className="home">
       <nav>
@@ -15,7 +54,9 @@ function Home() {
         </ul>
       </nav>
       <main>
-        <h2 className="title">List it out.</h2>
+        <animated.h2 className="title" style={titleProps} onClick={toggleList}>
+          {isList ? "List" : "Let"} it out.
+        </animated.h2>
         <h3 className="slogan">let it all out... *DIGITALLY*</h3>
       </main>
     </div>
@@ -23,3 +64,21 @@ function Home() {
 }
 
 export default Home;
+
+//   const titleProps = useSpring({
+//     from: { opacity: 0, transform: "translateY(-50px)" },
+//     to: { opacity: 1, transform: "translateY(0px)" },
+//     delay: 500,
+//   });
+
+//   const sloganProps = useSpring({
+//     from: { opacity: 0, transform: "translateX(-50px)" },
+//     to: { opacity: 1, transform: "translateX(0px)" },
+//     delay: 1000,
+//   });
+
+//   const navProps = useSpring({
+//     from: { opacity: 0, transform: "translateY(-50px)" },
+//     to: { opacity: 1, transform: "translateY(0px)" },
+//     delay: 1500,
+//   });
