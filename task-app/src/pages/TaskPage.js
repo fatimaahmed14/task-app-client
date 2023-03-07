69525c84b624ae45efc18d3a803b228cd8cfecb7
fatima <fxtimaahmed14@gmail.com>
2023-03-07 12:03:12 +0000

    working on task page => DELETE req working

--- task-app/src/pages/TaskPage.js
+++ task-app/src/pages/TaskPage.js
@@ -3,11 +3,67 @@
 // DELETE task on ecah individual task
 // PATCH task on each task aswell
 
-function TaskPage() {
+import { useState, useEffect } from "react";
+// import "./style/App.css";
+import { Link } from "react-router-dom";
+
+function TaskPage({ user }) {
+  const [tasks, setTasks] = useState([]);
+  const apiUrl = "http://localhost:4000";
+
+  useEffect(() => {
+    const token = localStorage.getItem("token");
+    fetch(`${apiUrl}/tasks`, {
+      method: "GET",
+      headers: { Authorization: `Bearer ${token}` },
+    })
+      .then((res) => res.json())
+      .then((data) => setTasks(data));
+  }, []);
+
+  const handleDelete = (id) => {
+    const token = localStorage.getItem("token");
+    fetch(`${apiUrl}/tasks/${id}`, {
+      method: "DELETE",
+      headers: { Authorization: `Bearer ${token}` },
+    }).then(() => {
+      const updatedTasks = tasks.filter((task) => task.id !== id);
+      setTasks(updatedTasks);
+    });
+  };
+
+  const handleComplete = console.log("hey this works");
+
   return (
-    <>
-      <div>hey</div>
-    </>
+    <div className="task-container">
+      <h1>Task Page</h1>
+      <div className="task-list">
+        {tasks.map((task) => (
+          <div key={task.id} className="task">
+            <h3>{task.title}</h3>
+            <p>{task.description}</p>
+            <p>{task.deadline}</p>
+            <div className="button-group">
+              <button
+                className="delete-button"
+                onClick={() => handleDelete(task.id)}
+              >
+                Delete
+              </button>
+              {!task.completed && (
+                <button
+                  className="complete-button"
+                  onClick={() => handleComplete(task.id)}
+                >
+                  Complete
+                </button>
+              )}
+            </div>
+          </div>
+        ))}
+      </div>
+      <Link to="/">Back to Dashboard</Link>
+    </div>
   );
 }
 
